909e37eb808a0b47313ebd181adf274c213e5793
Thor Jørgin <thorsekstra@gmail.com>
2023-02-09 15:17:54 +0100

    Multihtreading sucks, i have removed it since it is slower than single thread for large values of RUNS

--- src/main/java/week6/runner.java
+++ src/main/java/week6/runner.java
@@ -4,8 +4,6 @@ import week6.tools.Timing;
 
 import java.util.*;
 
-import static week6.runner.countCycles;
-import static week6.runner.runLoops;
 
 public class runner {
     public static List<Integer> permutationGenerator(int length) {
@@ -43,7 +41,7 @@ public class runner {
         return numberOfCycles;
     }
 
-    public static ArrayList<Integer> runLoops(int SIZE, int RUNS){
+    public static ArrayList<Integer> runLoops(int SIZE, int RUNS) {
         ArrayList<Integer> counts = new ArrayList<>(Collections.nCopies(SIZE, 0));
 
         for (int i = 0; i < RUNS; i++) {
@@ -72,77 +70,15 @@ public class runner {
     }
 
 
-    private static void threadedStatTest(int SIZE, int RUNS) {
-        int numberOfThreads = 4; // Number of threads
-        ArrayList<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < numberOfThreads; i++) {
-            countingThread thread
-                    = new countingThread(SIZE, RUNS / numberOfThreads);
-            threads.add(thread);
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        for (int i = 0; i < CountHolder.counts.size(); i++) {
-            int x = CountHolder.counts.get(i);
-            System.out.printf("%d: %d - %d %s", i, x, Math.round(((double) x / RUNS) * 100), "%\n");
-        }
-    }
-
     public static void main(String[] args) {
 //        testCounter();
         final int SIZE = 64;
-        final int RUNS = 100_000;
+        final int RUNS = 1000_000;
 
         Timing.start("single thread");
         statGenereator(SIZE, RUNS);
         Timing.end("single thread");
-
-        Timing.start("Multi thread");
-        threadedStatTest(SIZE, RUNS);
-        Timing.end("Multi thread");
     }
 }
 
-class CountHolder{
-    public static ArrayList<Integer> counts = null;
-
-    public static synchronized void addCounts(ArrayList<Integer> input){
-        if (counts == null){
-            counts = input;
-            return;
-        }
-        for (int i = 0; i < counts.size(); i++) {
-            int val = counts.get(i) + input.get(i);
-            counts.set(i, val);
-        }
-    }
-}
-
-class countingThread extends Thread {
-    private final int SIZE;
-    private final int RUNS;
-    private ArrayList<Integer> counts;
-
-    public countingThread(int SIZE, int RUNS) {
-        this.SIZE = SIZE;
-        this.RUNS = RUNS;
-    }
-
-    public void run() {
-        this.counts = runLoops(this.SIZE, this.RUNS);
-        saveCounts();
-    }
-
-    private synchronized void saveCounts(){
-        CountHolder.addCounts(this.counts);
-    }
-}
 
